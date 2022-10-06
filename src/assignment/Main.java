1b5e8bc889459bf832159ff823bc2a318bd50929
Rix95 <ricalrax@gmail.com>
2022-10-05 20:51:01 -0500

    We're learning

--- src/assignment/Main.java
+++ src/assignment/Main.java
@@ -4,6 +4,7 @@ public class Main {
     //Hello!
     //This is a test
     //What about backwards?
+    //Learning Javadabadu
     public static void main(String[] args) {
         Word the = new Word("the");
         System.out.println(the.getGlobalFrequency());
