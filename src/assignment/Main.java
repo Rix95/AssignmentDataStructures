1cd452b64a40148373180abcb684a5eab3f2cac4
Rix95 <ricalrax@gmail.com>
2022-10-05 19:26:00 -0500

    Created Word Class

--- src/assignment/Main.java
+++ src/assignment/Main.java
@@ -1,6 +1,7 @@
 package assignment;
 
 public class Main {
+    //Hello!
     public static void main(String[] args) {
         Word the = new Word("the");
         System.out.println(the.getGlobalFrequency());
