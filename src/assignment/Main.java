bfaa3f9a804cd62c55e99ecf0afb0be977cdb18f
Rix95 <95265236+Rix95@users.noreply.github.com>
2022-10-05 19:30:11 -0500

    Update Main.java
    Testing things

--- src/assignment/Main.java
+++ src/assignment/Main.java
@@ -3,6 +3,7 @@ package assignment;
 public class Main {
     //Hello!
     //This is a test
+    //What about backwards?
     public static void main(String[] args) {
         Word the = new Word("the");
         System.out.println(the.getGlobalFrequency());
