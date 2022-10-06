bf5cd023ecfc3c42a5495c725754633ea4e39b32
Rix95 <ricalrax@gmail.com>
2022-10-05 19:27:23 -0500

    testing branches

--- src/assignment/Main.java
+++ src/assignment/Main.java
@@ -2,6 +2,7 @@ package assignment;
 
 public class Main {
     //Hello!
+    //This is a test
     public static void main(String[] args) {
         Word the = new Word("the");
         System.out.println(the.getGlobalFrequency());
