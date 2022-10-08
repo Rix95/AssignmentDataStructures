d4a7b31b388c10dc83c8801cbc77a7f1dbbd9761
Rix95 <95265236+Rix95@users.noreply.github.com>
2022-10-08 18:22:47 -0500

    Update Main.java

--- src/assignment/Main.java
+++ src/assignment/Main.java
@@ -4,7 +4,7 @@ public class Main {
     //Hello!
     //This is a test
     //What about backwards?
-    //Learning Javadabadu today is october
+    //Learning Javadabadu
     public static void main(String[] args) {
         Word the = new Word("the");
         System.out.println(the.getGlobalFrequency());
