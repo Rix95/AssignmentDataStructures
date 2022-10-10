778c4cd42b22ed5352debde158d3bc738696eaef
Marissa <marissana4215@gmail.com>
2022-10-10 13:15:08 -0500

    its the truth

--- src/assignment/Main.java
+++ src/assignment/Main.java
@@ -6,7 +6,7 @@ public class Main {
     //What about backwards?
     //Learning Javadabadu
     public static void main(String[] args) {
-        Word the = new Word("marissa is awesome, meow meow");
+        Word the = new Word("hellooooooo");
         System.out.println(the.getGlobalFrequency());
 
     }
