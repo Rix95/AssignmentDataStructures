32a6ef9ca61b8a8db9904c304d978267f975d5bb
Rix95 <ricalrax@gmail.com>
2022-10-10 14:05:53 -0500

    WIP readfile
    Work in progress

--- src/assignment/Main.java
+++ src/assignment/Main.java
@@ -1,19 +1,33 @@
 package assignment;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.sql.Array;
+import java.util.ArrayList;
+
 public class Main {
-    //Hello!
-    //This is a test
-    //What about backwards?
-    //Learning Javadabadu
     public static void main(String[] args) {
+<<<<<<< Updated upstream
         Word the = new Word("the");
         System.out.println(the.getGlobalFrequency());
+=======
+        ArrayList<Sentence> arraySentence = new ArrayList<>;
+        ArrayList<Word> wordSentence = new ArrayList<>;
+>>>>>>> Stashed changes
 
     }
+
+<<<<<<< Updated upstream
+
+
+=======
+    public static void parseSentences(String textFile) throws FileNotFoundException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(textFile));
+        
+>>>>>>> Stashed changes
+
+    }
+
 }
-
-
-
-
-
-//This is a funny test line 19
