324892969a7481724b76f75f11ec9aa3f76f93ea
Rix95 <ricalrax@gmail.com>
2022-10-10 14:31:34 -0500

    Added Sentence parser, assign to arraySentence arrayList<Sentence>

--- src/assignment/Main.java
+++ src/assignment/Main.java
@@ -1,33 +1,36 @@
 package assignment;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
 import java.sql.Array;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
-<<<<<<< Updated upstream
-        Word the = new Word("the");
-        System.out.println(the.getGlobalFrequency());
-=======
-        ArrayList<Sentence> arraySentence = new ArrayList<>;
-        ArrayList<Word> wordSentence = new ArrayList<>;
->>>>>>> Stashed changes
+    public static void main(String[] args) throws IOException {
+        ArrayList<Sentence> arraySentence = parseSentences("src/data/tiny1.txt");
+        ArrayList<Word> wordSentence = new ArrayList<>();
+
+//        for (int i=0; i < arraySentence.size(); i++){
+//            System.out.println(arraySentence.get(i).getSentence());
+//            System.out.println("///////////");
+//        }
+
+        for (Sentence sentence : arraySentence){
+            System.out.println(sentence.getSentence());
+            System.out.println("///////////");
+        }
 
     }
 
-<<<<<<< Updated upstream
-
-
-=======
-    public static void parseSentences(String textFile) throws FileNotFoundException {
+    public static ArrayList<Sentence> parseSentences(String textFile) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new FileReader(textFile));
-        
->>>>>>> Stashed changes
-
+        String line;
+        ArrayList<Sentence> arraySentence = new ArrayList<>();
+        while ((line = bufferedReader.readLine()) != null) {
+           //System.out.println(line);
+           //System.out.println("/////////////////////");
+            arraySentence.add(new Sentence(line));
+        }
+        return arraySentence;
     }
 
 }
--- src/assignment/Sentence.java
+++ src/assignment/Sentence.java
@@ -2,6 +2,12 @@ package assignment;
 
 public class Sentence {
 
+    protected String sentence;
+    public Sentence(String sentence){
+        this.sentence = sentence;
+    }
 
-
+    public String getSentence(){
+        return this.sentence;
+    }
 }
