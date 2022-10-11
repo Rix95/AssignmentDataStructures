160954db413a24e535698b1da9ce50479430e379
Rix95 <ricalrax@gmail.com>
2022-10-11 00:36:54 -0500

    added some Sentence stuff and word interactions

--- src/assignment/Sentence.java
+++ src/assignment/Sentence.java
@@ -6,43 +6,73 @@ import java.util.Arrays;
 public class Sentence {
 
     private StringBuilder sentence;
-    private ArrayList<ArrayList<Object>> wordsInSentence = new ArrayList<>();
+    private ArrayList<ArrayList<Object>> wordFrequencyInSentenceList = new ArrayList<>();
+    private int highestFrequency = 0;
+    ArrayList<Word> wordsWithHighestFrequency = new ArrayList<>();
+
     public Sentence(StringBuilder sentence){
         this.sentence = sentence;
     }
 
     public void addWordWithFrequency(Word word){
         boolean found = false;
-        for(ArrayList<Object> wordFreq : wordsInSentence) {
-            if (wordFreq.get(0).equals(word)) {
-                int currentFreq = (int)(wordFreq.get(1));
-                currentFreq++;
+        int currentFreq =1;
+        for(ArrayList<Object> wordFreq : wordFrequencyInSentenceList) {
+            if (wordFreq.get(0) == word) {
+                currentFreq = (int)(wordFreq.get(1)) + 1;
                 wordFreq.set(1, currentFreq);
                 found = true;
-            }
-            if (found){
                 break;
-            }
+                }
+
         }
         if(!found){
-            wordsInSentence.add(new ArrayList<>(Arrays.asList(word, 1)));
+            wordFrequencyInSentenceList.add(new ArrayList<>(Arrays.asList(word, currentFreq)));
+        }
+        //Update Frequencies and wordsWithHighestFrequency!
+        if(currentFreq > highestFrequency){ //If current Frequency
+            highestFrequency = currentFreq;
+            wordsWithHighestFrequency.clear();
+            wordsWithHighestFrequency.add(word);
+        } else if(currentFreq == highestFrequency){  //If the word
+            wordsWithHighestFrequency.add(word);
         }
 
+    }
 
-        }
+    public int getHighestFrequency(){
+        return highestFrequency;
+    }
 
 
+    //Method to check if a word is in a sentence
+
+    //Return Sentence
     public StringBuilder getSentence(){
         return this.sentence;
     }
 
-    public Word getWordsWithHighestFrequency(){
-        //This method is a placeholder it does not produce intended result
-        Word word = new Word(new StringBuilder(), new Sentence(this.sentence));
-        return word;
+    public int getWordFrequency(Word word){
+        for(ArrayList<Object> wordInSentence : wordFrequencyInSentenceList){
+            if(wordInSentence.get(0) == word){
+                //Word wordFound = (Word)wordInSentence.get(0);
+                return (int)wordInSentence.get(1);
+            }
+        }
+        return 0;
     }
-    public ArrayList<ArrayList<Object>> getWordsInSentence(){
-        return wordsInSentence;
+    //This method will get the word/s with the highest frequency in a list,(1 or more)
+//    public ArrayList<Word> getWordsWithHighestFrequency(){
+//
+////        for(ArrayList<Object> wordList : wordFrequencyInSentenceList){
+////
+////
+////        }
+//
+//    }
+
+    public ArrayList<ArrayList<Object>> getWordFrequencyInSentenceList(){
+        return wordFrequencyInSentenceList;
     }
 
 
--- src/assignment/Word.java
+++ src/assignment/Word.java
@@ -7,10 +7,12 @@ public class Word {
     StringBuilder word;
     private int totalWordFrequency;
     protected ArrayList<Sentence> sentencesWithWord = new ArrayList<>();
+    protected static ArrayList<Word> ListOfAllWords = new ArrayList<>();
     public Word (StringBuilder word, Sentence sentence) {
         this.word = word;
         totalWordFrequency = 1;
         sentencesWithWord.add(sentence);
+        ListOfAllWords.add(this);
     }
 
     public int getTotalFrequency() {
@@ -20,12 +22,12 @@ public class Word {
     public void incrementWordFrequency() {
         totalWordFrequency++;
     }
-
+    
     public void addSentence(Sentence sentence){
     //For marissa
     }
 
-    public Sentence getSentenceWithHighestFrequency(ArrayList<Sentence> sentenceWord){
+    public Sentence getSentencesWithHighestFrequency(ArrayList<Sentence> sentenceWord){
         //This currently does not work
         Sentence sentence = new Sentence( new StringBuilder());
         return sentence;
