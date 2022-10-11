af5dd625c82361abc3c50776db107ecc929917e8
Rix95 <ricalrax@gmail.com>
2022-10-10 21:39:08 -0500

    Added Sentence parser, assign to arraySentence arrayList<Sentence>

--- src/assignment/Sentence.java
+++ src/assignment/Sentence.java
@@ -1,17 +1,50 @@
 package assignment;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Sentence {
 
-    protected StringBuilder sentence;
+    private StringBuilder sentence;
+    private ArrayList<ArrayList<Object>> wordsInSentence = new ArrayList<>();
     public Sentence(StringBuilder sentence){
-
         this.sentence = sentence;
     }
 
-    public StringBuilder getSentence(){
+    public void addWordWithFrequency(Word word){
+        boolean found = false;
+        for(ArrayList<Object> wordFreq : wordsInSentence) {
+            if (wordFreq.get(0).equals(word)) {
+                int currentFreq = (int)(wordFreq.get(1));
+                currentFreq++;
+                wordFreq.set(1, currentFreq);
+                found = true;
+            }
+            if (found){
+                break;
+            }
+        }
+        if(!found){
+            wordsInSentence.add(new ArrayList<>(Arrays.asList(word, 1)));
+        }
 
+
+        }
+
+
+    public StringBuilder getSentence(){
         return this.sentence;
     }
+
+    public Word getWordsWithHighestFrequency(){
+        //This method is a placeholder it does not produce intended result
+        Word word = new Word(new StringBuilder(), new Sentence(this.sentence));
+        return word;
+    }
+    public ArrayList<ArrayList<Object>> getWordsInSentence(){
+        return wordsInSentence;
+    }
+
 
 
 }
--- src/assignment/Word.java
+++ src/assignment/Word.java
@@ -6,10 +6,11 @@ public class Word {
 
     StringBuilder word;
     private int totalWordFrequency;
-    protected ArrayList<Sentence> sentenceWWord;
+    protected ArrayList<Sentence> sentencesWithWord = new ArrayList<>();
     public Word (StringBuilder word, Sentence sentence) {
         this.word = word;
         totalWordFrequency = 1;
+        sentencesWithWord.add(sentence);
     }
 
     public int getTotalFrequency() {
@@ -20,6 +21,20 @@ public class Word {
         totalWordFrequency++;
     }
 
+    public void addSentence(Sentence sentence){
+    //For marissa
+    }
+
+    public Sentence getSentenceWithHighestFrequency(ArrayList<Sentence> sentenceWord){
+        //This currently does not work
+        Sentence sentence = new Sentence( new StringBuilder());
+        return sentence;
+    }
+
+    @Override
+    public String toString() {
+        return word.toString();
+    }
 
 
 
