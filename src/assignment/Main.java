fbfbf922c52f76e2058f7bc52a086dc491332ac2
ediaz029 <ediazj03@gmail.com>
2022-10-12 17:32:01 -0500

    Added Sentence parser, assign to arraySentence arrayList<Sentence>

--- src/assignment/Main.java
+++ src/assignment/Main.java
@@ -8,17 +8,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws IOException {
         ArrayList<String> arrayLines = parseLines("src/data/tiny1.txt");
-        ArrayList<Word> wordSentence = new ArrayList<>();
+        //       ArrayList<Word> wordSentence = new ArrayList<>();
 //
-//        for(String s: arrayLines){
+//        for (String s : arrayLines) {
 //            System.out.println(s);
 //            System.out.println("///////////");
 //        }
 
         ArrayList<Sentence> arraySentences = parseSentences(arrayLines);
         System.out.println(arraySentences.size());
-        for(Sentence s : arraySentences){
-           System.out.println(s.getSentence());
+        for (Sentence s : arraySentences) {
+            System.out.println(s.getSentence());
         }
     }
 
@@ -27,33 +27,28 @@ public class Main {
         String line;
         ArrayList<String> arrayLines = new ArrayList<>();
         while ((line = bufferedReader.readLine()) != null) {
-           //System.out.println(line);
-           //System.out.println("/////////////////////");
-            arrayLines.add(new String(line));
+            arrayLines.add(new String(line.toLowerCase()));
         }
+
         return arrayLines;
     }
 
-    public static ArrayList<Sentence> parseSentences(ArrayList<String> arrayLines){
+    public static ArrayList<Sentence> parseSentences(ArrayList<String> arrayLines) {
 
         StringBuilder sentence = new StringBuilder();
 
         ArrayList<Sentence> arraySentences = new ArrayList<>();
 
-        for(String s : arrayLines){
-            //System.out.println(s.length());
-            for(int i =0; i < s.length(); i++){
-                //System.out.println(sentence);
-                if (s.charAt(i) == '.'){
-                    // System.out.println("Hello there sentence is "  + sentence + "char is" + s.charAt(i));
+        for (String s : arrayLines) {
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '.') {
                     arraySentences.add(new Sentence(sentence));
                     sentence = new StringBuilder();
-                }
-                else{
+                } else {
                     sentence.append(s.charAt(i));
                 }
             }
-            if (sentence.length() != 0){
+            if (sentence.length() != 0) {
                 arraySentences.add(new Sentence(sentence));
                 sentence = new StringBuilder();
             }
