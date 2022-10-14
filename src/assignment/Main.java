58c5cf0a0e646624127db13b9fa1acc9e64267b4
ediaz029 <ediazj03@gmail.com>
2022-10-13 19:22:04 -0500

    changed some variable names

--- src/assignment/Main.java
+++ src/assignment/Main.java
@@ -8,19 +8,41 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws IOException {
         ArrayList<String> arrayLines = parseLines("src/data/tiny1.txt");
-        //       ArrayList<Word> wordSentence = new ArrayList<>();
-//
-//        for (String s : arrayLines) {
-//            System.out.println(s);
-//            System.out.println("///////////");
-//        }
-
         ArrayList<Sentence> arraySentences = parseSentences(arrayLines);
-        //System.out.println(arraySentences.size());
-        for (Sentence s : arraySentences) {
-            //System.out.println(s.getSentence());
+        parseWords(arraySentences);
+        
+        
+//        for (Word word : Word.listOfAllWords){
+//            System.out.println(word.toString());
+//    }
+
+        ArrayList<Sentence> coolersentences = new ArrayList<>();
+        int highest = 0;
+        for(Sentence sentence : arraySentences){
+            if (sentence.HighestFrequencyInSentenceValue > highest) {
+                highest = sentence.HighestFrequencyInSentenceValue;
+                coolersentences.clear();
+                coolersentences.add(sentence);
+            }
+            else if(sentence.HighestFrequencyInSentenceValue == highest) {
+                coolersentences.add(sentence);
+            }
         }
-    }
+
+        for(Sentence wayCooler : coolersentences){
+            for (Word wordy : wayCooler.wordsWithHighestFrequencyInSentence){
+                System.out.println(wordy.toString());
+                System.out.println(wayCooler.HighestFrequencyInSentenceValue);
+                System.out.println(wayCooler.toString());
+            }
+        }
+
+        for(String words : Word.nthMostFrequentWord(3)){
+            System.out.println(words);
+        }
+        System.out.println(Word.rankingFrequency);
+
+        }
 
     public static ArrayList<String> parseLines(String textFile) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new FileReader(textFile));
@@ -29,16 +51,12 @@ public class Main {
         while ((line = bufferedReader.readLine()) != null) {
             arrayLines.add(new String(line.toLowerCase()));
         }
-
         return arrayLines;
     }
 
     public static ArrayList<Sentence> parseSentences(ArrayList<String> arrayLines) {
-
         StringBuilder sentence = new StringBuilder();
-
         ArrayList<Sentence> arraySentences = new ArrayList<>();
-
         for (String s : arrayLines) {
             for (int i = 0; i < s.length(); i++) {
                 if (s.charAt(i) == '.') {
@@ -52,8 +70,32 @@ public class Main {
                 arraySentences.add(new Sentence(sentence.toString()));
                 sentence = new StringBuilder();
             }
-
         }
         return arraySentences;
+    }
+
+    public static ArrayList<Word> parseWords(ArrayList<Sentence> sentences) {
+        StringBuilder word = new StringBuilder();
+
+        ArrayList<Word> words = new ArrayList<>();
+
+        for (Sentence sentence : sentences){
+            for(int i = 0; i < sentence.toString().length(); i++){
+                //System.out.println(s.charAt(i));
+                if(Character.isWhitespace(sentence.toString().charAt(i))) {
+                    if (word.length() != 0) {
+                        Word.addWord(word.toString(), sentence);
+                        word = new StringBuilder();
+                    }
+                }
+                else
+                    word.append(sentence.toString().charAt(i));
+            }
+            if(word.length() != 0){
+                Word.addWord(word.toString(), sentence);
+                word = new StringBuilder();
+            }
+        }
+        return words;
     }
 }
